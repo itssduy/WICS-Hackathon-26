82f4cf3d12c911c4d499e95582f3942c22960d5e
Duy <duypersonal512@gmail.com>
2026-03-21 05:44:25 -0500

    create question model

--- backend/src/main/java/com/example/demo/controller/HelloController.java
+++ backend/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,8 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Message;
-import com.example.demo.model.Quote;
-import com.example.demo.repository.QuoteRepository;
+import com.example.demo.model.Question;
+import com.example.demo.repository.QuestionRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,10 +10,10 @@ import java.util.List;
 @RestController
 public class HelloController {
 
-    private final QuoteRepository quoteRepository;
+    private final QuestionRepository questionRepository;
 
-    public HelloController(QuoteRepository quoteRepository) {
-        this.quoteRepository = quoteRepository;
+    public HelloController(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
     }
 
     @GetMapping("/hello")
@@ -28,13 +28,13 @@ public class HelloController {
 
     // these are just tests for the database connection
     // IGNORE THEM
-    @PostMapping("/test-quote")
-    public Quote addTestQuote(@RequestBody Quote quote) {
-        return quoteRepository.save(quote);
+    @PostMapping("/questions")
+    public Question addQuestion(@RequestBody Question question) {
+        return questionRepository.save(question);
     }
 
-    @GetMapping("/test-quotes")
-    public List<Quote> getTestQuotes() {
-        return quoteRepository.findAll();
+    @GetMapping("/questions")
+    public List<Question> getQuestions() {
+        return questionRepository.findAll();
     }
 }
