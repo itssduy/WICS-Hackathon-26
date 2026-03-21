561560acf9b89c0993e5cc75a07fa3a01ed6945a
Duy <duypersonal512@gmail.com>
2026-03-21 13:45:10 -0500

    redo input page

--- frontend/src/pages/input.jsx
+++ frontend/src/pages/input.jsx
@@ -1,28 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import '../styles/login.css';
 
 function Input (){
-    const [getInput, setInput] = useState("");
-    const [getConversation, setConversation] = useState([])
+    const [getUsername, setUsername] = useState("");
+    const [getGender, setGender] = useState("");
+    const [getCategory, setCategory] = useState("");
+    const [getTopics, setTopics] = useState("");
 
-    useEffect(()=>{
-       (async ()=>{
-        const apiURL = import.meta.env.VITE_API_URL;
-        //API URL NOT SET
-        const response = await fetch(`${apiURL}`, {
-            method: 'GET',
-            mode: "cors",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        
-
-        const data = await response.json();
-        setConversation(data);
-       })
-    }, [])
     const onSubmit = async (e) => {
         e.preventDefault();
         const apiURL = import.meta.env.VITE_API_URL;
@@ -34,29 +18,26 @@ function Input (){
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({ 
-                input: getInput, 
+                username: getUsername,
+                gender: getGender,
+                category: getCategory,
+                topics: getTopics
             }),
         });
-
-        
 
         const data = await response.json();
         console.log("Server Response:", data);
     }
     return(
         <>
-            <div>
-                <div>
-                   {getConversation.map((msg, index) => (
-                        <p key={index}>{msg}</p>
-                    ))} 
-                </div>
-                <form onSubmit={onSubmit}>
-                    <input type="text" placeholder="Input" onChange={(e)=>{setInput(e.target.value)}}/>
-                    <button type="submit">SEND SEND SEND</button>
-                </form>
-            </div>
-            
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="Username" onChange={(e)=>{setUsername(e.target.value)}}/>
+                <input type="text" placeholder="Gender" onChange={(e)=>{setGender(e.target.value)}}/>
+                <input type="text" placeholder="Category" onChange={(e)=>{setCategory(e.target.value)}}/>
+                <input type="text" placeholder="Topics" onChange={(e)=>{setTopics(e.target.value)}}/>
+
+                <button type="submit">Submit</button>
+            </form>  
         </>
     )
 }
