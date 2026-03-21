d84a81fec8e772eeea4584736fea13794310d0fb
Ivan <wan1976lee@outlook.com>
2026-03-21 04:30:36 -0500

    added json authresponse

--- backend/src/main/java/com/example/demo/controller/AuthorizationController.java
+++ backend/src/main/java/com/example/demo/controller/AuthorizationController.java
@@ -1,5 +1,5 @@
 package com.example.demo.controller;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.example.demo.model.AuthorizationRequest;
 import com.example.demo.model.SignUpRequest;
 import org.springframework.web.bind.annotation.*;
